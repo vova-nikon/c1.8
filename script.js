a0c5360618d878cef67af40e1bbb41d4e6fb35fc
vova-nikon <60705589+vova-nikon@users.noreply.github.com>
2020-05-08 22:02:11 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+function jQuery(selector, context = document) {
+	this.elements = Array.from(context.querySelectorAll(selector));
+	return this
+}
+
+jQuery.prototype.each = function (fn){
+	this.elements.forEach((element, index) => fn.call(element, element, index));
+	return this;
+}
+
+jQuery.prototype.click = function(fn){
+	this.each(element => element.addEventListener('click', fn))
+	return this
+}
+
+jQuery.prototype.remove = function(){
+	this.each(element => element.remove())
+  	return this;
+}
+
+jQuery.prototype.hide = function(){
+	this.each(element => element.style.display = 'none')
+  	return this;
+}
+
+jQuery.prototype.show = function(){
+	this.each(element => element.style.display = '')
+  	return this;
+}
+
+jQuery.prototype.class = function(name){
+    this.each(element => element.className = name)
+  	return this;
+}
+
+jQuery.prototype.html = function(html) {
+	if(html != undefined) {
+		return this.elements[0].innerHTML = html;
+	}
+	else {
+		return this.elements[0].innerHTML;
+	}
+}
+
+jQuery.prototype.text = function(text) {
+	if(text != undefined) {
+		return this.elements[0].innerText = text;
+	}
+	else {
+		return this.elements[0].innerText;
+	}
+}
+
+const $ = (e) => new jQuery(e);
